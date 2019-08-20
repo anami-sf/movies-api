dd9bfd7c41b5eb8b58da5cb5476c32fe8f7cd71a
anami823 <anami823@gmail.com>
2019-08-20 16:48:25 -0700

    Initial commit

---
+++ routes/api.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+const puppiesCtrl = require('../controllers/api/puppies')
+
+//Retrieve all puppies
+//router.get('/puppies', puppiesCtrl.index)
+
+//Add a puppy
+//router.get('/puppies', puppiesCtrl.create)
+
+//Show DETAILS for a single puppy by Id
+// router.get('/puppies:id', puppiesCtrl.show)
+
+//Update a single puppy by Id
+// router.get('/puppies:id', puppiesCtrl.update)
+
+//Delete a singel puppy by Id
+// router.get('/puppies:id', puppiesCtrl.remove)
+
+module.exports = router;
